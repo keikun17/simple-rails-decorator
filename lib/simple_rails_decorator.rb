98dd8371057121f2f26d2dd8837fe2208e8794e0
keikun17 <keikun17@gmail.com>
2013-05-27 01:47:41 +0800

    Implement.

--- lib/simple_rails_decorator.rb
+++ lib/simple_rails_decorator.rb
@@ -1,5 +1,17 @@
 require "simple_rails_decorator/version"
+require "delegate"
 
-module SimpleRailsDecorator
-  # Your code goes here...
+class SimpleDecorator
+  include ActionView::Helpers
+  include ActionView::Context
+  include Rails.application.routes.url_helpers
+
+  def self.decorate_collection(arr)
+    decorated_collection = []
+    arr.each do |item|
+      decorated_collection << self.new(item)
+    end
+    decorated_collection
+  end
+
 end
